b0b24dfdad0fb15eaa07a6588025829449994def
Janardhan Kumar <janardhankr2003@gmail.com>
2026-01-20 08:41:06 +0530

    01-FOLDER done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div id='parent'>
+      <h1 id='child1'>Hello madhav</h1>
+      <h1 id='child2'>Hello madhav</h1>
+    </div>
+  )
+}
+
+export default App
+
+
+
