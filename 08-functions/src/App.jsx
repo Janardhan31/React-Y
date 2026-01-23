34978754b0c1ea0b53422f89de061f67ca5e9ce4
Janardhan Kumar <janardhankr2003@gmail.com>
2026-01-23 21:03:46 +0530

    functions and map

---
+++ 08-functions/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const App = () => {
+
+  function inputchanging(val){
+    console.log(val)
+  }
+
+  return (
+    <div onWheel={
+      function(){
+        console.log('page scrolling')
+      }
+    }>
+    <div className='page1'></div>
+    <div className='page2'></div>
+    <div className='page3'></div>
+    </div>
+  )
+}
+
+export default App
