fe34f9bef8b8046f6f181020236c8f5603a2c078
Janardhan Kumar <janardhankr2003@gmail.com>
2026-02-02 21:13:42 +0530

    api-calliing

---
+++ 15-api-calling/src/App.jsx
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+const App = () => {
+
+  const getdata = ()=>{
+
+  }
+
+  return (
+    <div>
+      <button>Click Me</button>
+    </div>
+  )
+}
+
+export default App
